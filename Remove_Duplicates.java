08b7a771251639a5af335f997db8dda72f9a5a7f
rahul30824 <rahulr684.sse@saveetha.com>
2025-01-27 20:17:13 +0530

    Remove_Duplicates.java

--- Remove_Duplicates.java
+++ Remove_Duplicates.java
@@ -1,27 +1,24 @@
-import java.util.HashSet;
-class Main
-{
-    public static  int removeDuplicates(int[] array)
+class Main{
+    public static int removeDuplicates(int[] array)
     {
-    HashSet<Integer>set=new HashSet<>();
-    int j=0;
-    for(int i=0;i<array.length;i++)
-    {
-        if(!set.contains(array[i]))
-        {
-           set.add(array[i]);
-           array[j++]=array[i];
-        }
+        int j=0;
+       for(int i=0;i<array.length-1;i++) 
+       {
+           if(array[i]!=array[i+1])
+           {
+              array[j++] =array[i];
+           }
+           
+       }
+       array[j++]=array[array.length-1];
+       return j;
     }
-    return j;
-}
 public static void main(String[] args)
 {
-  int array[] ={1,2,2,3,4,4,4,5,5};
-  int result=removeDuplicates(array);
-  for(int j=0;j<result;j++)
-  {
-      System.out.print(array[j]+" ");
-  }
+    int [] array={1,2,2,3,3,4,5,5};
+    int result=removeDuplicates(array);
+    for(int i=0;i<result;i++){
+    System.out.print(array[i]);
+    }
 }
 }
